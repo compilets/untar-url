cee843641f86a4b4970ab9728a29edce7554eda5
Cheng <git@zcbenz.com>
2024-09-02 19:21:49 +0900

    Switch to use tar-mini

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,49 @@
 import {Readable} from 'node:stream';
+import {finished} from 'node:stream/promises';
+import {createGunzip} from 'node:zlib';
 import {createReadStream} from 'node:fs';
 import {mkdirSync} from 'node:fs';
 import {writeFile} from 'node:fs/promises';
 import {fileURLToPath} from 'node:url';
 
-import * as Throttle from 'promise-parallel-throttle';
-import {Tsunami} from './tsunami';
+import * as tar from 'tar-mini';
+import {newQueue} from '@henrygd/queue';
 
 interface UntarOptions {
   filter?: (fileName: string) => boolean;
-  fileTypes?: RegExp[],
 }
 
 export async function untar(url: string, targetDir: string, options?: UntarOptions) {
   // Fetch url.
-  let readable: ReadableStream;
+  let readable: Readable;
   if (new URL(url).protocol == 'file:') {
-    readable = Readable.toWeb(createReadStream(fileURLToPath(url))) as ReadableStream;
+    readable = createReadStream(fileURLToPath(url));
   } else {
     const response = await fetch(url);
     if (!response.ok || !response.body)
       throw new Error(`fetch failed with status code: ${response.status}`);
-    readable = response.body;
+    readable = Readable.fromWeb(response.body);
   }
   // Handle gzip decompression.
   if (url.endsWith('.gz') || url.endsWith('.tgz')) {
-    readable = readable.pipeThrough(new DecompressionStream('gzip'));
+    readable = readable.pipe(createGunzip());
   }
   // Parse the files.
-  const tsunami = new Tsunami(options?.fileTypes ?? [ /.*/ ], true);
-  const blob = await new Response(readable).blob();
-  await tsunami.untar(new File([ blob ], url));
-  // Write files.
-  const tasks = [];
-  for (const file of tsunami.files) {
+  const queue = newQueue(5);
+  const extract = tar.createExtract();
+  extract.on('entry', (head, file) => {
     // Filter files.
-    if (options?.filter && !options.filter(file.name)) {
-      continue;
+    if (options?.filter && !options.filter(head.name)) {
+      return;
     }
     // Create parent directory.
-    if (file.name.endsWith('/')) {
-      mkdirSync(`${targetDir}/${file.name}`, {recursive: true});
-      continue;
+    if (head.name.endsWith('/')) {
+      mkdirSync(`${targetDir}/${head.name}`, {recursive: true});
+      return;
     }
-    // Write file.
-    tasks.push(() => writeFile(`${targetDir}/${file.name}`, Buffer.from(file.buffer), {mode: file.mode}));
-  }
-  await Throttle.all(tasks);
+    // Write head.
+    queue.add(() => writeFile(`${targetDir}/${head.name}`, file, {mode: head.mode}));
+  });
+  await finished(readable.pipe(extract.receiver));
+  await queue.done();
 }
